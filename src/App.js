53b000b0d4e17ab4bb30241d8c8381f5385069e5
adrianescat <adrian.escat@gmail.com>
2016-04-09 18:36:00 -0300

    first components and composition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+var BugFilter = React.createClass({
+  render: function() {
+    return (
+    <div>Filter</div>
+    );
+  }
+});
+
+var BugTable = React.createClass({
+  render: function() {
+    return (
+    <div>a table to list all the bugs</div>
+    );
+  }
+});
+
+var BugAdd = React.createClass({
+  render: function() {
+    return (
+    <div>a form to add a new bug</div>
+    );
+  }
+});
+
+var BugList = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Bug Tracker</h1>
+        <BugFilter />
+        <hr />
+        <BugTable />
+        <hr />
+        <BugAdd />
+      </div>
+    );
+  }
+});
+ReactDOM.render(<BugList />, document.getElementById('main'));
