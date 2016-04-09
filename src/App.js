833ab0b310a9b68299f2fd4c7c53311a17974bcd
adrianescat <adrian.escat@gmail.com>
2016-04-09 19:16:25 -0300

    communicate between components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,35 @@ var BugFilter = React.createClass({
 var BugTable = React.createClass({
   render: function() {
     return (
-    <div>a table to list all the bugs</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Priority</th>
+            <th>Owner</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <BugRow id={1} status="closed" priority="urgent" owner="Federico" title="Unknow first() method for NIL class"/>
+          <BugRow id={2} status="open" priority="urgent" owner="Federico" title="Can't assing attributes"/>
+        </tbody>
+      </table>
+    );
+  }
+});
+
+var BugRow = React.createClass({
+  render: function() {
+    return (
+    <tr>
+      <td>{this.props.id}</td>
+      <td>{this.props.status}</td>
+      <td>{this.props.priority}</td>
+      <td>{this.props.owner}</td>
+      <td>{this.props.title}</td>
+    </tr>
     );
   }
 });
