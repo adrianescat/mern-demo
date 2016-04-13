1865541969cb3fe57bbbb8045b496ba27ffadf7b
adrianescat <adrian.escat@gmail.com>
2016-04-13 20:11:04 -0300

    filter Form

--- src/BugFilter.js
+++ src/BugFilter.js
@@ -2,13 +2,41 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var BugFilter = React.createClass({
+  getInitialState: function() {
+    return {status: "", priority: ""};
+  },
   _submit: function(e) {
-    this.props.submitHandler({priority: "P1"});
+    this.props.submitHandler({priority: this.state.priority, status: this.state.status});
+  },
+  _onChangeStatus: function(e) {
+    this.setState({status: e.target.value});
+  },
+  _onChangePriority: function(e) {
+    this.setState({priority: e.target.value});
   },
   render: function() {
     console.log("Rendering BugFilter");
     return (
-      <button onClick={this._submit}>Test Filter</button>
+      <div>
+        <h3>Filter</h3>
+        Status:
+        <select value={this.state.status} onChange={this._onChangeStatus}>
+          <option value="">(Any)</option>
+          <option value="New">New</option>
+          <option value="Open">Open</option>
+          <option value="Closed">Closed</option>
+        </select>
+        <br/>
+        Priority:
+        <select value={this.state.priority} onChange={this._onChangePriority}>
+          <option value="">(Any)</option>
+          <option value="P1">P1</option>
+          <option value="P2">P2</option>
+          <option value="P3">P3</option>
+        </select>
+        <br/>
+        <button onClick={this._submit}>Apply</button>
+      </div>
     )
   }
 });
